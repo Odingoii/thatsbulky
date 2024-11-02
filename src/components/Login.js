cc8a79ace027f2afee0dc503dcf7394c9a0b32a8
Soloh Odingo <124275153+Odingoii@users.noreply.github.com>
2024-11-02 05:35:34 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,15 @@
-// Login.js
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { loginUser } from '../api'; // Import the API call from api.js
+import { useAppContext, ACTIONS } from './App'; // Import context and actions
 import './Auth.css';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const { dispatch } = useAppContext(); // Access the context to dispatch actions
+    const navigate = useNavigate(); // Initialize the useNavigate hook
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -15,6 +18,11 @@ function Login() {
             const token = response.token; // Extract the token from response
             localStorage.setItem('token', token); // Store the token in local storage
             setMessage('Login successful');
+            
+            // Dispatch action to set loggedIn state
+            dispatch({ type: ACTIONS.SET_LOGGED_IN, payload: true });
+            // Redirect to SendMessage or QRCodeScanner based on your logic
+            navigate('/home'); // Redirect to home page after successful login
         } catch (error) {
             setMessage(error.response?.data?.error || 'Login failed');
         }
